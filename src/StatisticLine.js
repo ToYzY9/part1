621076187322882a254ac928a2109be536f283c9
BAbdoul <pad1934@hotmail.fr>
2024-09-02 01:43:47 +0200

    Affichage des statistiques dans un tableau HTML

--- src/StatisticLine.js
+++ src/StatisticLine.js
@@ -2,15 +2,36 @@ const StatisticLine = ({ text, count }) => {
     if (count === 0) return <p>{text}</p>;
     if (text === "Positive") {
         return (
-            <p>
-                {text} {count} %
-            </p>
+            <div>
+                <table>
+                    <thead></thead>
+                    <tbody>
+                        <tr>
+                            <td style={{ width: "60px" }}>{text}</td>
+                            <td>{count} %</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         );
     }
     return (
-        <p>
-            {text} {count}
-        </p>
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th> </th>
+                        <th> </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td style={{ width: "60px" }}>{text}</td>
+                        <td>{count}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     );
 };
 
