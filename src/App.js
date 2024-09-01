77669ce60b61a77cce9d68765d1dcf4f0c7db038
BAbdoul <pad1934@hotmail.fr>
2024-09-01 10:59:11 +0200

    L'application  affiche le nombre total de commentaires recueillis pour chaque catégorie.  L'application fonctionne que pendant une seule session de navigateur. Une fois la page actualisé, les commentaires recueillisson remis à zero et disparaisses.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import { useState } from "react";
 
-function App() {
-    const [counter, setCounter] = useState(0);
+const Button = (props) => {
+    return <button onClick={props.handleClick}>{props.text}</button>;
+};
+
+const Paragraph = ({ text, count }) => {
+    if (count === 0) {
+        return <p>{text}</p>;
+    }
     return (
-        <>
-            <h1>{counter}</h1>
-            <button onClick={() => setCounter(counter + 1)}>Plus</button>
-            <button onClick={() => setCounter(0)}>Zero</button>
-            <button onClick={() => setCounter(counter - 1)}>Moins</button>
-        </>
+        <p>
+            {text} {count}
+        </p>
+    );
+};
+
+function App() {
+    // enregistrer les clics de chaque bouton dans un état différent
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+
+    const handleAddGoodByOne = () => {
+        setGood(good + 1);
+    };
+    const handleAddNeutralByOne = () => () => {
+        setNeutral(neutral + 1);
+    };
+    const handleAddBadByOne = () => {
+        setBad(bad + 1);
+    };
+    return (
+        <div>
+            <h1>Give feedback</h1>
+            <Button handleClick={() => handleAddGoodByOne()} text="Good" />
+            <Button handleClick={handleAddNeutralByOne()} text="Neutral" />
+            <Button handleClick={handleAddBadByOne} text="Bad" />
+
+            <h2>Statistics</h2>
+            <Paragraph text="Good" count={good} />
+            <Paragraph text="Neutral" count={neutral} />
+            <Paragraph text="Bad" count={bad} />
+        </div>
     );
 }
 
